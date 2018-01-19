a394f64e86e58912e4c4729aac45deb5d5c0c154
Voltrage <ldanowskion@gmail.com>
2018-01-18 17:45:48 -0800

    initial commit 1/18/2018

---
+++ app/src/main/java/com/example/danowski19/myapplication/photo_fun.java
@@ -0,0 +1,13 @@
+package com.example.danowski19.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class photo_fun extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_fun);
+    }
+}
